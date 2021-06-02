1398e7bd6fe0a293cede8e65093061c60b1e98bb
Micha Moskovic <michamos@gmail.com>
2021-06-02 14:38:02 +0200

    setup: pin json-merger v0.7.4
    * workaround for problems introduces in json-merger v0.7.5, see
      inspirehep/inspirehep#1841.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ install_requires = [
     'munkres==1.0.12',
     'inspire-schemas~=61.0',
     'inspire-utils~=3.0,>=3.0.0',
-    'json-merger[contrib]~=0.0,>=0.6.0',
+    'json-merger[contrib]~=0.0,==0.7.4',
     'pyrsistent~=0.0,>=0.14.0',
 ]
 
